495fd1370cf2edbd1f39457483ba3a1b645190cb
Francisco Fernández <ferrycosv@gmail.com>
2020-03-19 18:02:04 +0100

    Show list of filtered items and copy selected item to input field

--- development-strategy.md
+++ development-strategy.md
@@ -26,6 +26,12 @@ Building this site one step at a time
 
 ## 3. User Story: `look-ahead`
 
-- A user can see suggestions of cities and state names based on what he is typing in the input text field.
+- A user receive suggestions of cities and state names based on what he is typing in the input text field.
 - I wrote the code in `listeners/look-ahead.js` & `handlers/look-ahead.js`
 - The handler function matches and filters the `cities` array with the text typed in the input box, after it shows the user a list with suggestions based on the matches found, the handler is connected to the UI with a `change` and `keyup` event.
+
+## 3. User Story: `show list`
+
+- A user can see and select from a list with suggestions of cities and state names based on what he is typing in the input text field.
+- I wrote the code in `handlers/look-ahead.js`
+- The handler function show the list of all the items matches on the screen, it also highlights the word matched from the suggestions and population number, the handler to select is connected to the UI with a `click` event for each list item.
--- handlers/look-ahead.js
+++ handlers/look-ahead.js
@@ -5,16 +5,54 @@ function findMatches(wordToMatch, cities) {
   });
 }
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function copyHandler() {
+  const inputField = document.querySelector(".search");
+  const suggestions = document.querySelector(".suggestions");
+  const newInput = this.children[0].innerText;
+  inputField.value = newInput;
+  suggestions.innerHTML = "";
+  
+  log.push({
+    handler: "copyHandler",
+    newInput
+  });
+}
+
 function showListHandler() {
   const newInput = this.value;
   const matchArray = findMatches(newInput, cities);
-  console.log(matchArray);
-  
+  const suggestions = document.querySelector(".suggestions");
+  const html = matchArray
+    .map(place => {
+      const regex = new RegExp(newInput, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${newInput}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${newInput}</span>`
+      );
+      return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+  for (let c of suggestions.children) {
+    c.addEventListener("click", copyHandler);
+  }
+
   log.push({
     handler: "showListHandler",
     newInput,
     cities: matchArray
   });
 }
-
-
